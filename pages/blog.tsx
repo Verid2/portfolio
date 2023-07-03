cac48059993514571cf3d137f16fd9860ccd7ef9
Gergely.Ficsur <muteddnb22@gmail.com>
2023-07-03 14:11:37 +0200

    adedd blog page

---
+++ pages/blog.tsx
@@ -0,0 +1,56 @@
+import { useRouter } from "next/router";
+
+export default function Blog(){
+
+    const router = useRouter();
+
+
+    const handleClick = (path: string) => {
+      router.push(path);
+    };
+
+return (
+    <>
+<div className="w-screen h-screen relative mx-60 bg-white">
+  <div className="w-[236px] h-[43px] left-[856px] top-[27px] absolute">
+    <div className="left-0 top-0 absolute text-black text-[20px] font-medium hover:underline cursor-pointer" onClick={()=>handleClick("/projects") }>Projects</div>
+    <div className="w-[106px] h-[43px] left-[26px] top-0 absolute text-right text-black text-[20px] font-medium hover:underline cursor-pointer" onClick={()=>handleClick("/blog") }>Blog</div>
+    <div className="w-[71px] h-[43px] left-[165px] top-0 absolute text-right text-black text-[20px] font-medium hover:underline cursor-pointer" onClick={()=>handleClick("/contact") }>Contact</div>
+  </div>
+  <div className="w-[156px] h-[54px] left-[235px] top-[160px] absolute text-black text-[44px] font-bold">Blog</div>
+  <div className="w-[305px] h-[108px] left-[424px] top-[1123px] absolute">
+    <div className="w-[50px] h-[46px] left-0 top-[4px] absolute justify-center items-center inline-flex">
+      <div className="w-[50px] h-[46px] relative">
+      </div>
+    </div>
+    <div className="left-[26px] top-[84px] absolute text-black text-[16px] font-normal">Copyright ©2023 All rights reserved</div>
+    <img className="w-[119px] h-[53px] left-[94px] top-0 absolute" src="https://via.placeholder.com/119x53" />
+  </div>
+  <div className="w-[682px] h-[165.27px] left-[235px] top-[269px] absolute">
+    <div className="w-[682px] h-[33px] left-0 top-0 absolute text-black text-[30px] font-medium">Blog post 1</div>
+    <div className="w-[157px] h-[29px] left-0 top-[59px] absolute text-black text-[20px] font-normal">2023.07.03</div>
+    <div className="w-[682px] h-[43px] left-0 top-[89px] absolute text-black text-[16px] font-normal">Lorem ipsum</div>
+    <div className="w-[681px] h-[0px] left-[1px] top-[161px] absolute border border-neutral-200"></div>
+  </div>
+  <div className="w-[682px] h-[165.27px] left-[235px] top-[462px] absolute">
+    <div className="w-[682px] h-[33px] left-0 top-0 absolute text-black text-[30px] font-medium">Blog post 2</div>
+    <div className="w-[157px] h-[29px] left-0 top-[59px] absolute text-black text-[20px] font-normal">2023.07.03</div>
+    <div className="w-[682px] h-[43px] left-0 top-[89px] absolute text-black text-[16px] font-normal">Lorem ipsum</div>
+    <div className="w-[681px] h-[0px] left-[1px] top-[161px] absolute border border-neutral-200"></div>
+  </div>
+  <div className="w-[682px] h-[165.27px] left-[235px] top-[665px] absolute">
+    <div className="w-[682px] h-[33px] left-0 top-0 absolute text-black text-[30px] font-medium">Blog post 2</div>
+    <div className="w-[157px] h-[29px] left-0 top-[59px] absolute text-black text-[20px] font-normal">2023.07.03</div>
+    <div className="w-[682px] h-[43px] left-0 top-[89px] absolute text-black text-[16px] font-normal">Lorem ipsum</div>
+    <div className="w-[681px] h-[0px] left-[1px] top-[161px] absolute border border-neutral-200"></div>
+  </div>
+  <div className="w-[682px] h-[165.27px] left-[235px] top-[848px] absolute">
+    <div className="w-[682px] h-[33px] left-0 top-0 absolute text-black text-[30px] font-medium">Blog post 2</div>
+    <div className="w-[157px] h-[29px] left-0 top-[59px] absolute text-black text-[20px] font-normal">2023.07.03</div>
+    <div className="w-[682px] h-[43px] left-0 top-[89px] absolute text-black text-[16px] font-normal">Lorem ipsum</div>
+    <div className="w-[681px] h-[0px] left-[1px] top-[161px] absolute border border-neutral-200"></div>
+  </div>
+</div>
+</>
+)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,70 @@
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
+
+
+  const handleClick = (path: string) => {
+    router.push(path);
+  };
+
   return (
     <>
-    <div className="w-screen h-screen relative mx-60 bg-white">
-  <div className="w-[57px] h-[43px] left-[856px] top-[27px] absolute text-black text-[20px] font-medium">Work</div>
-  <div className="w-[106px] h-[43px] left-[882px] top-[27px] absolute text-right text-black text-[20px] font-medium">Blog</div>
-  <div className="w-[71px] h-[43px] left-[1021px] top-[27px] absolute text-right text-black text-[20px] font-medium">Contact</div>
-  <div className="w-[506px] h-[105px] left-[148px] top-[228px] absolute text-black text-[44px] font-bold">Hi, I am Gergő,<br/>Junior Developer</div>
-  <div className="w-[243px] h-[243px] left-[755px] top-[223px] absolute bg-slate-100 rounded-full" />
-  <img className="w-[255px] h-[243px] left-[760px] top-[210px] absolute rounded-full" src="/geriboy.png" />
-  <div className="w-[497px] h-[66px] left-[148px] top-[373px] absolute text-black text-[16px] font-normal">At the moment I’m a solo developer . This is my portfolio page for recruiting. In this page you can follow my blog, where I will try to update frequently where am I, in my coding adventure.</div>
-  <div className="w-52 h-[47px] left-[148px] top-[477px] absolute bg-red-400 rounded-sm" />
-  <div className="w-[180px] h-[21px] left-[168px] top-[486px] absolute text-white text-[20px] font-medium">Download Resume</div>
-  <div className="w-[1152px] h-[396px] left-0 top-[595px] absolute bg-slate-100" />
-  <div className="w-[140px] h-[42px] left-[148px] top-[602px] absolute text-black text-[22px] font-normal">Recent posts</div>
-  <div className="w-52 h-5 left-[796px] top-[622px] absolute text-right text-cyan-500 text-[16px] font-medium">View all</div>
-  <div className="w-[418px] h-[295px] left-[148px] top-[664px] absolute bg-white rounded" />
-  <div className="w-[418px] h-[295px] left-[586px] top-[664px] absolute bg-white rounded" />
-  <div className="w-[374px] h-[85px] left-[171px] top-[688px] absolute text-black text-[26px] font-bold">Blog post 1</div>
-  <div className="w-[164px] h-[21px] left-[171px] top-[790px] absolute text-black text-[18px] font-normal">2023.07.02</div>
-  <div className="w-[368px] h-[117px] left-[171px] top-[835px] absolute text-black text-[16px] font-normal">Lorem ipsum</div>
-  <div className="w-[374px] h-[85px] left-[609px] top-[688px] absolute text-black text-[26px] font-bold">Blog post 2</div>
-  <div className="w-[164px] h-[21px] left-[609px] top-[790px] absolute text-black text-[18px] font-normal">2023.07.02</div>
-  <div className="w-[368px] h-[117px] left-[609px] top-[835px] absolute text-black text-[16px] font-normal">Lorem ipsum</div>
-  <div className="w-[152px] h-[60px] left-[148px] top-[1006px] absolute text-center text-black text-[22px] font-normal">Featured works</div>
-  <img className="w-[244px] h-[177px] left-[146px] top-[1085px] absolute rounded-md" src="/synth.png" />
-  <div className="w-[858px] h-[0px] left-[146px] top-[1283px] absolute border border-neutral-200"></div>
-  <div className="left-[410px] top-[1077px] absolute text-black text-[30px] font-bold">Python Synthesizer</div>
-  <div className="w-[62px] h-[60px] left-[410px] top-[1123px] absolute">
-  <div className="w-[62px] h-[25px] left-0 top-[18px] absolute bg-blue-950 rounded-2xl" />
-  <div className="left-[10px] top-[10px] absolute text-white text-[18px] font-black leading-10">2023</div>
-  </div>
-  <div className="w-[594px] h-[93px] left-[410px] top-[1188px] absolute text-black text-[16px] font-normal">This is my love project a little and simple synthesizer what am I working on.  It doesn’t have too much features right now but it will improve in a few months hopefully.</div>
-  <img className="w-[244px] h-[177px] left-[146px] top-[1314px] absolute rounded-md" src="/foxshop.png" />
-  <div className="w-[858px] h-[0px] left-[146px] top-[1529px] absolute border border-neutral-200"></div>
-  <div className="left-[410px] top-[1312px] absolute text-black text-[30px] font-bold">School Project (FoxShop)</div>
-  <div className="w-[62px] h-[60px] left-[410px] top-[1354px] absolute">
-    <div className="w-[62px] h-[25px] left-0 top-[18px] absolute bg-blue-950 rounded-2xl" />
-    <div className="left-[10px] top-[10px] absolute text-white text-[18px] font-black leading-10">2023</div>
-  </div>
-  <div className="w-[594px] h-[93px] left-[410px] top-[1419px] absolute text-black text-[16px] font-normal">This was my project in the school were i learnt the basics of programming. We get to know JavaScript,Typescript,C# and SQL.<br/>In this application we built a little used market where you can upload various advertisements.</div>
-  <img className="w-[244px] h-[177px] left-[146px] top-[1560px] absolute rounded-md" src="/netflix-clone.png" />
-  <div className="left-[410px] top-[1560px] absolute text-black text-[30px] font-bold">Netflix Clone</div>
-  <div className="w-[62px] h-[60px] left-[420px] top-[1599px] absolute">
-    <div className="w-[62px] h-[25px] left-0 top-[18px] absolute bg-blue-950 rounded-2xl" />
-    <div className="left-[10px] top-[10px] absolute text-white text-[18px] font-black leading-10">2023</div>
-  </div>
-  <div className="w-[594px] h-[93px] left-[413px] top-[1679px] absolute text-black text-[16px] font-normal">This is a site where i would like to recrete a netflix styled streaming platform ( I’m using a tutorial for this but i would like to add my features).<br/>It’s still in progress I want to finish it as soon as possible.</div>
-  <div className="w-[858px] h-[0px] left-[146px] top-[1776px] absolute border border-neutral-200"></div>
-  <img className="w-[45px] h-[37px] left-[437px] top-[1904px] absolute" src="/insta.svg" />
-  <img className="w-[45px] h-[37px] left-[700px] top-[1904px] absolute" src="/Linkedin.svg"/>
-  <img className= "w-[55px] h-[55px] left-[568px] top-[1895px] absolute"  src="/icons8-email-48.png"/>
-  <div>
-    <div className="w-[50px] h-[46px] left-0 top-0 absolute">
-    </div>
-  </div>
-  <div className="left-[467px] top-[1965px] absolute text-black text-[16px] font-normal">Copyright ©2023 All rights reserved</div>
-</div>
+      <div className="w-screen h-screen relative mx-60 bg-white">
+        <div className="w-[57px] h-[43px] left-[856px] top-[27px] absolute text-black text-[20px] font-medium hover:underline cursor-pointer" onClick={() => handleClick("/projects")}>Projects</div>
+        <div className="w-[106px] h-[43px] left-[882px] top-[27px] absolute text-right text-black text-[20px] font-medium hover:underline cursor-pointer" onClick={() => handleClick("/blog")}>Blog</div>
+        <div className="w-[71px] h-[43px] left-[1021px] top-[27px] absolute text-right text-black text-[20px] font-medium hover:underline cursor-pointer">Contact</div>
+        <div className="w-[506px] h-[105px] left-[148px] top-[228px] absolute text-black text-[44px] font-bold">Hi, I am Gergő,<br />Junior Developer</div>
+        <div className="w-[260px] h-[243px] left-[755px] top-[223px] absolute bg-slate-100 rounded-full" />
+        <img className="w-[255px] h-[243px] left-[760px] top-[210px] absolute rounded-full" src="/geriboy.png" />
+        <div className="w-[497px] h-[66px] left-[148px] top-[373px] absolute text-black text-[16px] font-normal">At the moment I’m a solo developer . This is my portfolio page for recruiting. In this page you can follow my blog, where I will try to update frequently where am I, in my coding adventure.</div>
+        <div className="w-52 h-[47px] left-[148px] top-[477px] absolute bg-red-400 rounded-sm" />
+        <div className="w-[180px] h-[21px] left-[168px] top-[486px] absolute text-white text-[20px] font-medium">Download Resume</div>
+        <div className="w-[1152px] h-[396px] left-0 top-[595px] absolute bg-slate-100" />
+        <div className="w-[140px] h-[42px] left-[148px] top-[602px] absolute text-black text-[22px] font-normal">Recent posts</div>
+        <div className="w-52 h-5 left-[796px] top-[622px] absolute text-right text-cyan-500 text-[16px] font-medium">View all</div>
+        <div className="w-[418px] h-[295px] left-[148px] top-[664px] absolute bg-white rounded" />
+        <div className="w-[418px] h-[295px] left-[586px] top-[664px] absolute bg-white rounded" />
+        <div className="w-[374px] h-[85px] left-[171px] top-[688px] absolute text-black text-[26px] font-bold">Blog post 1</div>
+        <div className="w-[164px] h-[21px] left-[171px] top-[790px] absolute text-black text-[18px] font-normal">2023.07.02</div>
+        <div className="w-[368px] h-[117px] left-[171px] top-[835px] absolute text-black text-[16px] font-normal">Lorem ipsum</div>
+        <div className="w-[374px] h-[85px] left-[609px] top-[688px] absolute text-black text-[26px] font-bold">Blog post 2</div>
+        <div className="w-[164px] h-[21px] left-[609px] top-[790px] absolute text-black text-[18px] font-normal">2023.07.02</div>
+        <div className="w-[368px] h-[117px] left-[609px] top-[835px] absolute text-black text-[16px] font-normal">Lorem ipsum</div>
+        <div className="w-[152px] h-[60px] left-[148px] top-[1006px] absolute text-center text-black text-[22px] font-normal">Featured projects</div>
+        <img className="w-[244px] h-[177px] left-[146px] top-[1085px] absolute rounded-md" src="/synth.png" />
+        <div className="w-[858px] h-[0px] left-[146px] top-[1283px] absolute border border-neutral-200"></div>
+        <div className="left-[410px] top-[1077px] absolute text-black text-[30px] font-bold">Python Synthesizer</div>
+        <div className="w-[62px] h-[60px] left-[410px] top-[1123px] absolute">
+          <div className="w-[62px] h-[25px] left-0 top-[18px] absolute bg-blue-950 rounded-2xl" />
+          <div className="left-[10px] top-[10px] absolute text-white text-[18px] font-black leading-10">2023</div>
+        </div>
+        <div className="w-[594px] h-[93px] left-[410px] top-[1188px] absolute text-black text-[16px] font-normal">This is my love project a little and simple synthesizer what am I working on.  It doesn’t have too much features right now but it will improve in a few months hopefully.</div>
+        <img className="w-[244px] h-[177px] left-[146px] top-[1314px] absolute rounded-md" src="/foxshop.png" />
+        <div className="w-[858px] h-[0px] left-[146px] top-[1529px] absolute border border-neutral-200"></div>
+        <div className="left-[410px] top-[1312px] absolute text-black text-[30px] font-bold">School Project (FoxShop)</div>
+        <div className="w-[62px] h-[60px] left-[410px] top-[1354px] absolute">
+          <div className="w-[62px] h-[25px] left-0 top-[18px] absolute bg-blue-950 rounded-2xl" />
+          <div className="left-[10px] top-[10px] absolute text-white text-[18px] font-black leading-10">2023</div>
+        </div>
+        <div className="w-[594px] h-[93px] left-[410px] top-[1419px] absolute text-black text-[16px] font-normal">This was my project in the school were i learnt the basics of programming. We get to know JavaScript,Typescript,C# and SQL.<br />In this application we built a little used market where you can upload various advertisements.</div>
+        <img className="w-[244px] h-[177px] left-[146px] top-[1560px] absolute rounded-md" src="/netflix-clone.png" />
+        <div className="left-[410px] top-[1560px] absolute text-black text-[30px] font-bold">Netflix Clone</div>
+        <div className="w-[62px] h-[60px] left-[420px] top-[1599px] absolute">
+          <div className="w-[62px] h-[25px] left-0 top-[18px] absolute bg-blue-950 rounded-2xl" />
+          <div className="left-[10px] top-[10px] absolute text-white text-[18px] font-black leading-10">2023</div>
+        </div>
+        <div className="w-[594px] h-[93px] left-[413px] top-[1679px] absolute text-black text-[16px] font-normal">This is a site where i would like to recrete a netflix styled streaming platform ( I’m using a tutorial for this but i would like to add my features).<br />It’s still in progress I want to finish it as soon as possible.</div>
+        <div className="w-[858px] h-[0px] left-[146px] top-[1776px] absolute border border-neutral-200"></div>
+        <img className="w-[45px] h-[37px] left-[437px] top-[1904px] absolute" src="/insta.svg" />
+        <img className="w-[45px] h-[37px] left-[700px] top-[1904px] absolute" src="/Linkedin.svg" />
+        <img className="w-[55px] h-[55px] left-[568px] top-[1895px] absolute" src="/icons8-email-48.png" />
+        <div>
+          <div className="w-[50px] h-[46px] left-0 top-0 absolute">
+          </div>
+        </div>
+        <div className="left-[467px] top-[1965px] absolute text-black text-[16px] font-normal">Copyright ©2023 All rights reserved</div>
+      </div>
     </>
   )
 }
